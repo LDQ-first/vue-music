19800650464d55ed4182683082f08b5ee600bcdd
LDQ-first <178848890@qq.com>
2017-11-11 16:13:07 +0800

    'modify'

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <!--<router-view/>-->
+    <player></player>
+  </div>
+</template>
+
+<script>
+
+import MHeader from './components/m-header/m-header.vue'
+import Tab from './components/tab/tab.vue'
+import Player from './components/player/player.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MHeader,
+    Tab,
+    Player
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="m-header">
+      <div class="icon"></div>
+      <h1 class="text">Vue Music</h1>
+     <!-- <router-link tag="div" to="/user" class="mine">
+        <i class="icon-mine"></i>
+      </router-link>-->
+    </div>
+</template>
+
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="stylus">
+
+  @import "../../common/stylus/variable"
+  @import "../../common/stylus/mixin"
+
+  .m-header
+    position: relative
+    height: 44px
+    text-align: center
+    color: $color-theme
+    font-size: 0
+    .icon
+      display: inline-block
+      vertical-align: top
+      margin-top: 8px
+      width: 28px
+      height: 30px
+      margin-right: 9px
+      background-image: url('./logo.png')
+      background-size: 28px 30px
+    .text
+      display: inline-block
+      vertical-align: top
+      line-height: 44px
+      font-size: $font-size-large
+    .mine
+      position: absolute
+      top: 0
+      right: 0
+      .icon-mine
+        display: block
+        padding: 12px
+        font-size: 20px
+        color: $color-theme
+
+</style>
